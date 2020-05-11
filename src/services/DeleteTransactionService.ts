df93b13ab175e4ea7fd0897003cd0b0d53f8aae1
John Freitas <john.freitasau@gmail.com>
2020-05-11 18:07:55 +1000

    List, Create and Delete transactions & Upload CSV

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Unable to delete: User not found.');
+    }
+
+    await transactionRepository.delete(id);
   }
 }
 
